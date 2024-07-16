48fafbfabc7d1a6ae2d6d13fc97bdc8c2dcd165e
Jeditale <69350223+Jeditale@users.noreply.github.com>
2024-07-17 03:44:37 +0700

    I put it in wrong folder

--- lab 5-1/hanoi.js
+++
@@ -1,9 +0,0 @@
-const hanoi = (n, from, to, aux) => {
-    if (n === 0) return;
-    hanoi(n - 1, from, aux, to);
-    console.log(`${from} --> ${to}`);
-    hanoi(n - 1, aux, to, from);
-};
-
-// Test the function with 3 disks
-hanoi(3, 'A', 'C', 'B');
