d1f3f82ed8d4eb7123de74092e8077ae6a72f641
Jeditale <69350223+Jeditale@users.noreply.github.com>
2024-07-22 10:18:50 +0700

    Finish

--- lab6-2/app.js
+++ lab6-2/app.js
@@ -1,22 +1,12 @@
-const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
-const { buildSchema } = require('graphql');
-app.use(express.static('public'));
-app.get('/', (req, res) => {
-res.send('hello world');
-});
-app.getUser('/user', function (req, res) {
-res.send('Got a GET request') //for get data
-});
-app.userPost('/user', function (req, res) {
-res.send('Got a POST request') // for add
-});
-app.put('/userAdd', function (req, res) {
-res.send('Got a PUT request at user') // for update
-});
-app.delete('/userDelete', function (req, res) {
-res.send('Got a DELETE request at user') // for delete
-});
-app.listen(3000, () => {
-console.log('Server is running on port 3000');
-});
+const express = require('express'); 
+const app = express(); 
+ 
+app.use(express.static('public')); //create folder public for place file html 
+ 
+app.get('/', (req, res) => { 
+  res.send('hello world'); 
+}); 
+ 
+app.listen(3000, () => { 
+  console.log('Server is running on port 3000'); 
+}); 
